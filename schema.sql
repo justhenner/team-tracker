5ef668dc064864c6fb73c0ebed427b70802cd6d3
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-02-03 22:48:26 -0500

    expanded seeds and schema files

--- schema.sql
+++ schema.sql
@@ -3,22 +3,24 @@ CREATE DATABASE team_db;
 
 USE team_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    name VARCHAR(30),
+    department_name VARCHAR(30),
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  
     first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT, 
+    last_name VARCHAR(30), 
     manager_id INT, 
+    FOREIGN KEY (role_id)
 );
+
---
+++ seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO departments (department_name)
+VALUES  ("sales"),
+        ("engineering"),
+        ("finance"),
+        ("legal");
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES  ("Salesperson", "80000"),
+        ("Lead Engineer", "150000"),
+        ("Software Engineer", "120000"),
+        ("Account Manager", "160000"),
+        ("Accountant", "125000"),
+        ("Legal Team Lead", "250000"),
+        ("Lawyer", "190000");
+
+INSERT INTO employees (first_name, last_name)
+VALUES  ("Mike", "Chan"), 
+        ("Ashley", "Rodriguez"),
+        ("Kevin", "Tupik"), 
+        ("Kunal", "Singh"),
+        ("Malia", "Brown"),
+        ("Sarah", "Lourd"),
+        ("Tom", "Allen");
