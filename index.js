93a38227b1c45f4afca426eebdaa52236caa007a
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-11 18:50:51 -0500

    created addRole function

--- index.js
+++ index.js
@@ -18,3 +18,35 @@ function addDepartment() {
         startsPrompts();
     });
 }
+
+const addRole = () => {
+    let department = new Department();
+    department.getDepartments().then((department) => {
+        return inquirer 
+        .prompt([
+            {
+                name: "title",
+                type: "input", 
+                message: "What title does the role hold?",
+            },
+            {
+                name: "salary", 
+                type: "input", 
+                message: "What salary does the role offer?",
+            },
+            {
+                name: "department_name",
+                type: "list",
+                message: "What department does the role fall under?",
+                choices: department.map((d) => {
+                    return `${d.id} ${d.name}`;
+                }),
+            },
+        ]) 
+        .then(function ({ title, salary, department_name }) {
+            const role = new Role(null, title, salary, department_name);
+            role.addRole;
+            startsPrompts();
+        });
+    });
+};
